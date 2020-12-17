7509dde4d60856c7a23c40eeead0da28963ad182
adamtlee <adam.lee346@gmail.com>
2020-12-17 15:55:40 -0700

    In progress file reader and output writer

---
+++ IOReader/CsvFileProcessor.cs
@@ -0,0 +1,34 @@
+﻿using CsvHelper;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+
+namespace IOReader
+{
+    class CsvFileProcessor
+    {
+        public string InputFilePath { get;  }
+        public string OutputFilePath { get;  }
+
+        public CsvFileProcessor(string inputFilePath, string outputFilePath)
+        {
+            InputFilePath = inputFilePath;
+            OutputFilePath = outputFilePath;
+        }
+
+        public void Process()
+        {
+            using (StreamReader input = File.OpenText(InputFilePath))
+            using (CsvReader csvReader = new CsvReader(input, System.Globalization.CultureInfo.CurrentCulture))
+            {
+                IEnumerable<dynamic> records = csvReader.GetRecords<dynamic>();
+                csvReader.Configuration.HasHeaderRecord = false;
+                foreach (var record in records)
+                {
+                    Console.WriteLine(record.Id);
+                }    
+            }
+        }
+    }
+}
